4bbe98d77c38c6036107f2b5052765f076a78355
olivia / lee soyoung <59822833+olivia-soy@users.noreply.github.com>
2022-06-17 17:56:26 +0900

    init commit

--- app/src/main/java/com/olivia/architecturetemplate/MainActivity.kt
+++ app/src/main/java/com/olivia/architecturetemplate/MainActivity.kt
@@ -1,18 +1,121 @@
 package com.olivia.architecturetemplate
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.olivia.architecturetemplate.databinding.ActivityMainBinding
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.olivia.architecturetemplate.presentation.ui.favorite.Favorite
+import com.olivia.architecturetemplate.presentation.ui.home.Home
+import com.olivia.architecturetemplate.presentation.ui.theme.FavoriteAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContent {
+            FavoriteAppTheme {
+
+                HomeScreen()
+
+            }
+        }
     }
+}
+
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+private fun HomeScreen() {
+    val navController = rememberNavController()
+    Scaffold(bottomBar = { BottomNavigationBar(navController) }) {
+        NavigationHost(navController)
+    }
+}
+
+@Composable
+fun BottomNavigationBar(navController: NavHostController) {
+    NavigationBar {
+        val backStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = backStackEntry?.destination?.route
+        NavBarItems.BarItems.forEach { navItem ->
+            NavigationBarItem(
+                icon = {
+                    Icon(
+                        imageVector = navItem.image,
+                        contentDescription = navItem.title
+                    )
+                },
+                label = {
+                    Text(text = navItem.title)
+                },
+                selected = currentRoute == navItem.route,
+                onClick = {
+                    navController.navigate(navItem.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
+        }
+    }
+}
+
+
+@Composable
+private fun NavigationHost(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = NavRoutes.Home.route,
+    ) {
+        composable(NavRoutes.Home.route) {
+            Home()
+        }
+
+        composable(NavRoutes.Favorites.route) {
+            Favorite()
+        }
+    }
+}
+
+sealed class NavRoutes(val route: String) {
+    object Home : NavRoutes("home")
+    object Favorites : NavRoutes("favorites")
+}
+
+data class BarItem(
+    val title: String,
+    val image: ImageVector,
+    val route: String
+)
+
+object NavBarItems {
+    val BarItems = listOf(
+        BarItem(
+            title = "Home",
+            image = Icons.Filled.Home,
+            route = "home"
+        ),
+        BarItem(
+            title = "Favorites",
+            image = Icons.Filled.Favorite,
+            route = "favorites"
+        )
+    )
 }
